8f67e8ac0b17a061423230754e35ec05ebe9d7d8
Andy Fang <AndyFang.DZ@gmail.com>
2014-08-01 00:09:12 +0800

    Add entire toolchain. Main program pending.

--- src/libhistogram/modules.py
+++ src/libhistogram/modules.py
@@ -1,5 +1,14 @@
 import cv2
 
 class Ghost(object):
-    def __init__(self, partition):
-        mask = cv2.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
+    def __init__(self, partition, mask):
+        self.mask = mask
+        hist = cv2.calcHist(partition, [0], mask, [16], [0, 180])
+        cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
+        self.hist = hist.reshape(-1)
+
+    def getHist(self):
+        return self.hist
+
+    def getBackProj(self, frame):
+        return cv2.calcBackProject([frame], [0], self.hist, [0, 180], 1)
---
+++ src/utils/mark.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import math
+
+def draw_machine_mark(size, location, image):
+    '''
+    This function draws a machine mark on the image
+    '''
+    original = cv2.imread("assets/machine.png", -1)
+
+    osize = math.sqrt(original.size)/2
+    ratio = size / osize
+    height, width, depth = image.shape
+    timg = cv2.resize(original, (0,0), fx=ratio, fy=ratio)
+    x, y = location 
+
+    image[:] *= 0.8
+
+    x_start = max(0, x - size/2)
+    x_end = min(x_start + size, width)
+    x_start = min(x_end - size, x_start)
+    y_start = max(0, y - size/2)
+    y_end = min(y_start + size, height)
+    y_start = min(y_end - size, y_start)
+    
+    roi = image[y_start : y_end, x_start : x_end]
+    for c in range(0,3):
+        roi[:,:,c] = timg[:,:,c] * (timg[:,:,3] / 255.0) + roi[:,:,c] * (1 - timg[:,:,3] / 255.0) 
+    image[y_start : y_end, x_start : x_end] = roi
